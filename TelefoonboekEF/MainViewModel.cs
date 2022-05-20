de8e763193b25c19887a4de5e3fc9b0db509e674
Matthias Druwé <matthias.druwe@gmail.com>
2022-05-20 17:59:51 +0200

    Update

---
+++ TelefoonboekEF/MainViewModel.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Telefoonboek
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string lastname;
+        private string firstname;
+        private string email;
+        private string phoneNumber;
+        private ObservableCollection<PhoneBookItem> phoneBookItems = new ObservableCollection<PhoneBookItem>();
+        private PhoneBookItem selectedItem;
+
+        public string Lastname
+        {
+            get { return lastname; }
+            set
+            {
+                lastname = value;
+                OnPropertyChanged();
+                CheckSaveButtonAvailablity();
+            }
+        }
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set
+            {
+                firstname = value;
+                OnPropertyChanged();
+                CheckSaveButtonAvailablity();
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged();
+                CheckSaveButtonAvailablity();
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set
+            {
+                phoneNumber = value;
+                OnPropertyChanged();
+                CheckSaveButtonAvailablity();
+            }
+        }
+
+        public PhoneBookItem SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                selectedItem = value;
+                if (selectedItem != null)
+                {
+                    FillTextBoxes(selectedItem);
+                }
+                else
+                {
+                    ResetProperties();
+                }
+                OnPropertyChanged();
+                CheckDeleteButtonAvailablity();
+            }
+        }
+
+        private bool saveButtonEnabled;
+        private bool deleteButtonEnabled;
+
+        public bool SaveButtonEnabled
+        {
+            get { return saveButtonEnabled; }
+            private set
+            {
+                saveButtonEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool DeleteButtonEnabled
+        {
+            get { return deleteButtonEnabled; }
+            private set
+            {
+                deleteButtonEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+
+        public ICommand SaveCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+
+        public ObservableCollection<PhoneBookItem> PhoneBookItems { get => phoneBookItems; set => phoneBookItems = value; }
+
+        public MainViewModel()
+        {
+            SaveCommand = new ActionCommand(SaveCommandAction);
+            DeleteCommand = new ActionCommand(DeleteCommandAction);
+        }
+
+        private void FillTextBoxes(PhoneBookItem item)
+        {
+            Lastname = item.Lastname;
+            Firstname = item.Firstname;
+            Email = item.Email;
+            PhoneNumber = item.PhoneNumber;
+        }
+
+        private void SaveCommandAction()
+        {
+            if (SelectedItem == null)
+            {
+                PhoneBookItems.Add(new PhoneBookItem(Lastname, Firstname, Email, PhoneNumber));
+                ResetProperties();
+            }
+            else
+            {
+                SelectedItem.Lastname = Lastname;
+                SelectedItem.Firstname = Firstname;
+                SelectedItem.Email = Email;
+                SelectedItem.PhoneNumber = PhoneNumber;
+
+                SelectedItem = null; // deselecteren van item.
+            }
+        }
+
+        private void ResetProperties()
+        {
+            Lastname = null;
+            Firstname = null;
+            Email = null;
+            PhoneNumber = null;
+        }
+
+        private void DeleteCommandAction()
+        {
+            PhoneBookItems.Remove(SelectedItem);
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void CheckSaveButtonAvailablity()
+        {
+            SaveButtonEnabled = (Lastname != null && Lastname != "") && (Firstname != null && Firstname != "") && ((Email != null && Email != "") || (PhoneNumber != null && PhoneNumber != ""));
+        }
+
+        private void CheckDeleteButtonAvailablity()
+        {
+            DeleteButtonEnabled = SelectedItem != null;    
+        }
+
+    }
+}
---
+++ TelefoonboekEF/PhoneBookItem.cs
@@ -0,0 +1,70 @@
+﻿    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    namespace Telefoonboek
+    {
+        internal class PhoneBookItem : INotifyPropertyChanged
+        {
+            private string lastname;
+            private string firstname;
+            private string email;
+            private string phoneNumber;
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public string Lastname
+            {
+                get { return lastname; }
+                set
+                {
+                    lastname = value;
+                    OnPropertyChanged();
+                }
+            }
+            public string Firstname
+            {
+                get { return firstname; }
+                set
+                {
+                    firstname = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public string Email
+            {
+                get { return email; }
+                set
+                {
+                    email = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public string PhoneNumber
+            {
+                get { return phoneNumber; }
+                set
+                {
+                    phoneNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public PhoneBookItem(string lastname, string firstname, string email, string phoneNumber)
+            {
+                Lastname = lastname;
+                Firstname = firstname;
+                Email = email;
+                PhoneNumber = phoneNumber;
+            }
+
+            private void OnPropertyChanged([CallerMemberName] string property = null)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(property));
+                }
+            }
+        }
+    }
